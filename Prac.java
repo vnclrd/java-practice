2269ac2e6a9feb26f82c7ee84a7f67d71667ac13
ivancalarde1301 <ivan.calarde1301@gmail.com>
2025-07-15 12:43:46 +0800

    Added boolean

--- Prac.java
+++ Prac.java
@@ -1,11 +1,27 @@
 public class Prac {
     public static void main(String[] args) {
+
         String fullName = "Miguel Ivan Calarde";
         int age = 24;
+
         System.out.println("Hello, World!");
         System.out.println("My name is Ivan and I am learning Java.");
+
+        System.out.println("");
+
         System.out.println("This is my full name:");
         System.out.println(fullName);
         System.out.println("This is my age when I created this: " + age);
+
+        System.out.println("");
+
+        // Need to review the following code
+        double androidShare = 81.7;
+        System.out.println(androidShare);
+
+        System.out.println("");
+
+        boolean intsCanHoldDecimals = false;
+        System.out.println(intsCanHoldDecimals);
     }
 }
