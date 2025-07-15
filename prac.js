07c70c92ad81f091840aee12a7cea80094bdd708
ivancalarde1301 <ivan.calarde1301@gmail.com>
2025-07-15 17:55:51 +0800

    Added practice for javascript

---
+++ prac.js
@@ -0,0 +1,40 @@
+console.log(5);
+
+// console.log(60)
+
+/*
+console.log(60)
+*/
+
+// LESSON: Data Types
+
+console.log(10 + 10);
+console.log(10 - 10);
+console.log(10 * 10);
+console.log(10 / 10);
+console.log(10 % 10);
+
+console.log('Hello' + 'World');
+console.log('Hello' + ' ' + 'World');
+
+console.log('Codecademy'.toUpperCase());
+console.log('    Remove whitespace   '.trim());
+
+console.log(Math.random() * 100); // Generates a random number and multiply to 100
+console.log(Math.floor(Math.random() * 100)); // Math.floor rounds the number down to the nearest whole number
+
+console.log(Math.ceil(43.8)); // Round up to the nearest whole integer
+console.log(Number.isInteger(2017)); // Check if the number is an integer
+
+// LESSON: Variables
+
+// var - cannot be changed
+var favoriteFood = 'pizza';
+var numOfSlices = 8;
+console.log(favoriteFood);
+console.log(numOfSlices);
+
+// let - can be changed
+let changeMe = true;
+changeMe = false;
+console.log(changeMe);
