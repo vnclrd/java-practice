59757b80a47e11752fb34301ed2b1698ee473c5c
ivancalarde1301 <ivan.calarde1301@gmail.com>
2025-07-15 12:09:51 +0800

    Java practice

---
+++ Prac.java
@@ -0,0 +1,11 @@
+public class Prac {
+    public static void main(String[] args) {
+        String fullName = "Miguel Ivan Calarde";
+        int age = 24;
+        System.out.println("Hello, World!");
+        System.out.println("My name is Ivan and I am learning Java.");
+        System.out.println("This is my full name:");
+        System.out.println(fullName);
+        System.out.println("This is my age when I created this: " + age);
+    }
+}
