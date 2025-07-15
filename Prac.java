63f2a490e4b7616cf12a4c02d5aa603d764a4bec
ivancalarde1301 <ivan.calarde1301@gmail.com>
2025-07-15 13:07:29 +0800

    Added static checking

--- Prac.java
+++ Prac.java
@@ -6,7 +6,6 @@ public class Prac {
 
         System.out.println("");
 
-        // string
         String fullName = "Miguel Ivan Calarde";
         System.out.print("This is my full name: ");
         System.out.print(fullName);
@@ -19,7 +18,7 @@ public class Prac {
 
         System.out.println("");
 
-        // double - need to review the following code
+        // double (int that can hold decimals)
         double androidShare = 81.7;
         System.out.println(androidShare);
 
@@ -28,11 +27,49 @@ public class Prac {
         // boolean
         boolean intsCanHoldDecimals = false;
         System.out.println(intsCanHoldDecimals);
-        
+
         System.out.println("");
 
         // char
         char expectedGrade = 'A';
         System.out.println(expectedGrade);
+
+        System.out.println("");
+        
+        System.out.println("\"Hello World\"");
+
+        System.out.println("");
+
+        System.out.println("This is a backslash: \\");
+
+        System.out.println("");
+
+        System.out.println("Hello\nGoodbye");
+        /*
+        Prints
+        Hello
+        Goodbye
+        */
+
+        System.out.println("");
+
+        // String
+        String openingLyrics = "Yesterday, all my troubles seemed so far away";
+        System.out.println(openingLyrics);
+
+        System.out.println("");
+
+        // static checking
+        int year = 2001;
+        String title = "Shrek";
+        char genre = 'C';
+        double runtime = 1.58;
+        boolean isPG = true;
+
+        System.out.println("Year: " + year);
+        System.out.println("Title: " + title);
+        System.out.println("Genre: " + genre);
+        System.out.println("Runtime: " + runtime + " hours");
+        System.out.println("Is PG: " + isPG);
     }
 }
