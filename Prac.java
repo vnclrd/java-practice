b7fcd897bfceab67b188dd1d25c878896f47e2ec
ivancalarde1301 <ivan.calarde1301@gmail.com>
2025-07-15 12:51:58 +0800

    Added int

--- Prac.java
+++ Prac.java
@@ -1,27 +1,38 @@
 public class Prac {
     public static void main(String[] args) {
 
-        String fullName = "Miguel Ivan Calarde";
-        int age = 24;
-
         System.out.println("Hello, World!");
         System.out.println("My name is Ivan and I am learning Java.");
 
         System.out.println("");
 
-        System.out.println("This is my full name:");
-        System.out.println(fullName);
+        // string
+        String fullName = "Miguel Ivan Calarde";
+        System.out.print("This is my full name: ");
+        System.out.print(fullName);
+
+        System.out.println("");
+
+        // int
+        int age = 24;
         System.out.println("This is my age when I created this: " + age);
 
         System.out.println("");
 
-        // Need to review the following code
+        // double - need to review the following code
         double androidShare = 81.7;
         System.out.println(androidShare);
 
         System.out.println("");
 
+        // boolean
         boolean intsCanHoldDecimals = false;
         System.out.println(intsCanHoldDecimals);
+        
+        System.out.println("");
+
+        // char
+        char expectedGrade = 'A';
+        System.out.println(expectedGrade);
     }
 }
